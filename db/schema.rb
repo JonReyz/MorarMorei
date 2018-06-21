a3e8e06668bf5187d7cfcc672a575db4934c28ee
Cezar <cezarguimaraes2810@gmail.com>
2018-06-21 00:22:29 -0300

    markers resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_144110) do
+ActiveRecord::Schema.define(version: 2018_06_21_030133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "markers", force: :cascade do |t|
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "description"
+    t.boolean "positive"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nome"
