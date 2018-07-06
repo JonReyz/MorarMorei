de471ee5d55a88b0462436a6cd9f53d48731bbc1
brunoxd <bruno.mendes.costa@usp.br>
2018-07-06 02:21:52 -0300

    Validação do cadastro de usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-	has_secure_password
+	has_secure_password message: "Insira um Password Válido"
+	validates_uniqueness_of :email, message: "E-mail já foi cadastrado"
+	validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Formato de e-mail inválido"}
+	validates_presence_of :nome, message: 'Campo nome não pode ser deixado em branco'
+	validates_presence_of :email, message: 'Campo nome não pode ser deixado em branco'
 end
