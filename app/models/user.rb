70d4c705c9ab0d243d467ec3fbe754113203a3c1
Jon Reyz <joaoreyz@gmail.com>
2018-07-06 03:22:30 -0300

    Correção de erros nas validações

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password message: "Insira um Password Válido"
-	validates_uniqueness_of :email, message: "E-mail já foi cadastrado"
-	validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Formato de e-mail inválido"}
-	validates_presence_of :nome, message: 'Campo nome não pode ser deixado em branco'
-	validates_presence_of :email, message: 'Campo nome não pode ser deixado em branco'
+	validates_uniqueness_of :email, message: "já foi cadastrado"
+	validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "inválido"}
+	validates_presence_of :nome, message: 'não pode ser deixado em branco'
+	validates_presence_of :email, message: 'não pode ser deixado em branco'
 end
